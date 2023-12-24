bdb951e6cff4e17090b03295765f615adfefb8c7
Gorgi07 <gorgi07@yandex.ru>
2023-12-24 18:41:17 +0300

    Добавлен вывод строки в main файл Добавлен атрибут __repr__ в класс Egor

--- classes.py
+++ classes.py
@@ -5,7 +5,11 @@ class Egor:
     def __str__(self):
         return f"Егор со статусом {self.status}"
 
+    def __repr__(self):
+        return f"Егор со статусом {self.status}"
+
 
 if __name__ == '__main__':
     egor2007 = Egor("Разработчик")
     print(egor2007)
+    print(egor2007.__repr__())
