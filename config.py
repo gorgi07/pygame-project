c43bdbcc969932c7b7352348b8d638a9d75a249e
Gorgi07 <gorgi07@yandex.ru>
2023-12-28 16:38:54 +0300

    изменен шаг (скорость) персонажа

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from load_images import load_image
 FPS = 50
 WIDTH = 64 * 8
 HEIGHT = 64 * 6
-STEP = 20
+STEP = 16
 GRAVITY = 3.5
 tile_images = {'wall': load_image('down_wall.png'), 'empty': load_image('font.png')}
 tile_width = tile_height = 64
