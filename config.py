1699787f7bd02f6fce702c89cc2cbc4e3cc92baa
Gorgi07 <gorgi07@yandex.ru>
2024-01-03 17:05:42 +0300

    Изменен размер окна игры

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 from load_images import load_image
 
 FPS = 50
-WIDTH = 64 * 14
-HEIGHT = 64 * 9
+WIDTH = 64 * 16
+HEIGHT = 64 * 12
 STEP = 16
 GRAVITY = 4.5
-tile_images = {'wall': load_image('down_wall.png'), 'empty': load_image('font.png')}
+tile_images = {'wall': load_image('down_wall.png'),
+               'empty': load_image('font.png')}
 tile_width = tile_height = 64
