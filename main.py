b312b90efddd35359b32ef8cd2899d77ea7256c4
Gorgi07 <gorgi07@yandex.ru>
2024-01-08 20:52:58 +0300

    Добавлен обработчик экстренного выключения

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
-from game_functions import start_screen
+from game_functions import start_screen, screen_clear, terminate
 from screen import screen
 
 pygame.init()
 pygame.key.set_repeat(200, 70)
 screen = screen
 
-start_screen()
+try:
+    start_screen()
+except KeyboardInterrupt:
+    terminate()
+finally:
+    screen_clear()
